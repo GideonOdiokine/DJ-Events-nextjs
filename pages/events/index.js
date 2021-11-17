8a043ca9e9c22d7023fc786167c5f337d2ea8566
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-18 00:32:11 +0100

    done with adding img to edit

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,9 +2,8 @@ import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 
-const PER_PAGE = 2
+const PER_PAGE = 2;
 export default function EventsPage({ events }) {
-
   return (
     <Layout>
       <h2> Events</h2>
@@ -17,13 +16,20 @@ export default function EventsPage({ events }) {
 }
 
 export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
 
+  // Fetch total Event
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = totalRes.json();
 
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}`);
+  // Fetch Events
+  const res = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
   const events = await res.json();
 
   return {
-    props: { events },
-
+    props: { events, page: +page, total },
   };
 }
