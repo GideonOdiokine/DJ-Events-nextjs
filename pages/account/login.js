18926ddda55985b670ace9f99c1c7a5a83d3752e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-18 07:58:18 +0100

    working on login page

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,3 +1,13 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Layout from "@/components/Layout";
+import { useState, useEffect, useContext } from "react";
+import { FaUser } from "react-icons/fa";
+import Link from 'next/link'
+import styles from '@/styles/AuthForm.module.css'
+
+
+
 export default function login() {
     return (
         <div>
