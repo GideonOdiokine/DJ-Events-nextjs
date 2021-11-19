5bb382281af8be3aa02167bc5b4998b8404f5f95
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-19 14:06:33 +0100

    imported authContext to header for authentication

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
+import { useContext } from "react";
 import styles from "@/styles/Header.module.css";
+import AuthContext from "@/context/AuthContext";
 import Search from "./Search";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -15,17 +19,26 @@ export default function Header() {
           <li>
             <Link href="/events">Events</Link>
           </li>
-          <li>
-            <Link href="/events/add">Add Event</Link>
-          </li>
-
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href="/events/add">Add Event</Link>
+              </li>
+              <li>
+                <button className="btn-secondary btn-icons" >< FaSignOutAlt />Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
